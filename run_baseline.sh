8822bd7b7be6a138af061a018fb1289ed0426ea7
linhld152179 <39935721+linhld0811@users.noreply.github.com>
2025-02-14 17:17:02 +0700

    Update run_baseline.sh

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,5 +1,5 @@
-language=("ar" "de" "en" "es" "fr" "ja" "pt "zh)
+language=("ar" "de" "en" "es" "fr" "ja" "pt" "zh")
 for lang in "${language[@]}";do
-    python dstc12/bot_adversarial_dialogue/LlamaGuard.py --input_file dstc12/bot_adversarial_dialogue/$lang
+    python LlamaGuard.py --input_file dstc12/bot_adversarial_dialogue/$lang
     echo "Done $lang"
 done
